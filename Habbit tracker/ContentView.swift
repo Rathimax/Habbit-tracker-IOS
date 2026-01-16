fc626afd70dc283a187fc7d63cb4b1743cdd243c
Rathimax <abhayrajrathi616@gmail.com>
2026-01-16 19:42:37 +0530

    Initial Commit

---
+++ Habbit tracker/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Habbit tracker
+//
+//  Created by Abhay Raj Rathi on 16/01/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Habbit tracker/Habbit_trackerApp.swift
@@ -0,0 +1,17 @@
+//
+//  Habbit_trackerApp.swift
+//  Habbit tracker
+//
+//  Created by Abhay Raj Rathi on 16/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Habbit_trackerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
